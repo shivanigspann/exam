10335568abdb19628508da5247b4ec126a11f754
yc26rn1 <randy.nott@macys.com>
2014-10-15 11:54:29 -0700

    LSA-542 Implement meeting sort ordering.

--- src/main/webapp/scripts/dashboard/models/meetingList.js
+++ src/main/webapp/scripts/dashboard/models/meetingList.js
@@ -9,12 +9,41 @@ define([
 
 		url: 'turnin/meetings',
 
-		/* set model type used for this collection */
+		// model type used for this collection
         model: Model,
 
-        /* comparator determines how collection is sorted
-        comparator: 'authorLast'
-         */
+        // sorting
+        comparator: function(a, b) {
+            // project order
+            var aValue = a.get('project').id;
+            var bValue = b.get('project').id;
+            if (aValue < bValue) {
+            	return -1;
+            } else if (aValue > bValue) {
+            	return 1;
+            }
+
+            // meeting date order
+            aValue = a.get('date');
+            bValue = b.get('date');
+            if (aValue < bValue) {
+            	return -1;
+            } else if (aValue > bValue) {
+            	return 1;
+            }
+
+            // meeting name order
+            aValue = a.get('name');
+            bValue = b.get('name');
+            if (aValue < bValue) {
+            	return -1;
+            } else if (aValue > bValue) {
+            	return 1;
+            }
+
+            // duplicates not expected but...
+            return 0;
+        }
     });
 
 	return MeetingList;
